39b51a795375679aa6d9564d65cc3f8e89a18ec4
Kshitij Patil <kshitijpatil98@gmail.com>
2024-06-16 21:05:11 +0530

    Add offset handling in the http Handle function

--- backend/api/feed_handler.go
+++ backend/api/feed_handler.go
@@ -1,9 +1,16 @@
 package api
 
 import (
+	"encoding/json"
 	"github.com/Kshitij09/snakechat_server/data/model"
+	"github.com/Kshitij09/snakechat_server/util"
 	"net/http"
+	"reflect"
 )
+
+type TrendingFeedRequest struct {
+	Offset string `json:"offset"`
+}
 
 type TrendingFeedResponse struct {
 	Posts  []FeedPost `json:"posts"`
@@ -21,10 +28,27 @@ type FeedPost struct {
 	Downloads int64
 }
 
-func (s *Server) handleGetTrendingFeed(w http.ResponseWriter, _ *http.Request) error {
-	feed, err := s.db.Feed.GetFirstTrendingFeed()
-	if err != nil {
-		return err
+func (s *Server) handleGetTrendingFeed(w http.ResponseWriter, r *http.Request) error {
+	var feed *model.Feed
+	var dbErr error
+	if r.Body == http.NoBody {
+		feed, dbErr = s.db.Feed.GetFirstTrendingFeed()
+	} else {
+		req := TrendingFeedRequest{}
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			return err
+		}
+		if reflect.ValueOf(req.Offset).IsZero() {
+			feed, dbErr = s.db.Feed.GetFirstTrendingFeed()
+		} else {
+			feed, dbErr = s.db.Feed.GetTrendingFeed(req.Offset)
+		}
+	}
+	if dbErr != nil {
+		return dbErr
+	}
+	if feed == nil {
+		return util.SimpleAPIError(http.StatusNotFound, "Feed not found for given parameters")
 	}
 	resp := TrendingFeedResponse{Posts: toApiFeedPosts(feed.Posts), Offset: feed.Offset}
 	return WriteSuccessJson(w, resp)
