7498c8ef14bab2e5362314c2d7f90fcb488121cb
Kshitij Patil <kshitijpatil98@gmail.com>
2024-07-01 02:23:51 +0530

    Cleanup likers specific implementation
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- backend/domain/likers.go
+++ backend/domain/likers.go
@@ -1,22 +1,14 @@
 package domain
 
 import (
-	"encoding/base64"
 	"errors"
 	"fmt"
 	"github.com/Kshitij09/snakechat_server/domain/paging"
-	"log"
-	"strconv"
 )
 
 const LikersPageSize = 40
 
 var ErrInvalidLikersOffset = errors.New("invalid offset")
-
-type LikersPage struct {
-	Offset *string
-	Likers []Liker
-}
 
 type Liker struct {
 	Id             string
@@ -70,61 +62,4 @@ func (s *LikersService) CommentLikers(commentId string, offset *string) (*paging
 		return nil, fmt.Errorf("CommentLikers: %w", err)
 	}
 	return likers, nil
-}
-
-type likersFetcher struct {
-	byId          likersById
-	byIdAndOffset likersByIdAndOffset
-}
-
-type likersById func(id string) ([]Liker, error)
-type likersByIdAndOffset func(id string, offset int64) ([]Liker, error)
-
-func (fetcher likersFetcher) fetchPage(id string, offset *string) (*LikersPage, error) {
-	likers, err := fetcher.fetch(id, offset)
-	if err != nil {
-		return nil, err
-	}
-	var nextOffset *string
-	if len(likers) == LikersPageSize {
-		nextOffset = nextOffsetOrNil(likers)
-	}
-	return &LikersPage{Likers: likers, Offset: nextOffset}, nil
-}
-
-func (fetcher likersFetcher) fetch(id string, offset *string) ([]Liker, error) {
-	if offset == nil || len(*offset) == 0 {
-		return fetcher.byId(id)
-	} else {
-		offsetInt, err := parseOffset(*offset)
-		if err != nil {
-			return nil, err
-		}
-		return fetcher.byIdAndOffset(id, *offsetInt)
-	}
-}
-
-func parseOffset(offset string) (*int64, error) {
-	decodedBytes, err := base64.StdEncoding.DecodeString(offset)
-	if err != nil {
-		log.Printf("error decoding offset: %s\n", err)
-		return nil, ErrInvalidLikersOffset
-	}
-	decoded := string(decodedBytes)
-	offsetInt, err := strconv.ParseInt(decoded, 10, 64)
-	if err != nil {
-		log.Printf("error parsing offset: %s\n", err)
-		return nil, ErrInvalidLikersOffset
-	}
-	return &offsetInt, nil
-}
-
-func nextOffsetOrNil(likers []Liker) *string {
-	if len(likers) == 0 {
-		return nil
-	}
-	last := likers[len(likers)-1]
-	updateTimestamp := strconv.FormatInt(last.UpdatedAt, 10)
-	encoded := base64.StdEncoding.EncodeToString([]byte(updateTimestamp))
-	return &encoded
 }
