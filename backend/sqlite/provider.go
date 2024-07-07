51c546f6982c30548fa2cb0a0e491713b0ecb8e8
Kshitij Patil <kshitijpatil98@gmail.com>
2024-07-07 17:09:20 +0530

    [BE] Validate environment paths (#19)
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- backend/sqlite/provider.go
+++ backend/sqlite/provider.go
@@ -8,6 +8,11 @@ import (
 )
 
 func New() (*sql.DB, error) {
+	dbPath := os.Getenv("DB_PATH")
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		return nil, errors.New("database path does not exist: " + dbPath)
+	}
 	db, err := sql.Open("sqlite3", os.Getenv("DB_PATH"))
 	if err != nil {
 		envWarnErr := errors.New("error opening the DB connection, make sure you have set `DB_PATH` environment variable")
