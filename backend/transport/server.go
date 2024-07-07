8a6a1a8c9de043a21545af4055f5c0ca21b4f852
Kshitij Patil <kshitijpatil98@gmail.com>
2024-07-07 17:08:20 +0530

    Add ciphersuite for http2
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- backend/transport/server.go
+++ backend/transport/server.go
@@ -79,6 +79,7 @@ func (s *Server) Run(port int) error {
 				CipherSuites: []uint16{
 					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 					tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
 					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 				},
