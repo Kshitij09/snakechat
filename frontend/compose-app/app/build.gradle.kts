66ae51cb34210b38a19225b99ddd5d9cae2e0384
Kshitij Patil <kshitijpatil98@gmail.com>
2024-07-22 17:36:46 +0530

    Generate dependencies baseline
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- frontend/compose-app/app/build.gradle.kts
+++ frontend/compose-app/app/build.gradle.kts
@@ -50,3 +50,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
+
+dependencyGuard {
+    configuration("prodReleaseRuntimeClasspath")
+}
