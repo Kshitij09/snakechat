e554ad927572f3e35d9d612121b29cd32bbf35a8
Kshitij Patil <kshitijpatil98@gmail.com>
2024-07-28 00:28:16 +0530

    Fix Text component color
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- frontend/compose-app/ui/design/src/main/kotlin/cc/snakechat/design/Text.kt
+++ frontend/compose-app/ui/design/src/main/kotlin/cc/snakechat/design/Text.kt
@@ -1,11 +1,9 @@
 package cc.snakechat.design
 
 import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -17,7 +15,6 @@ import androidx.compose.ui.text.style.TextOverflow
 fun SnakeText(
     text: String,
     modifier: Modifier = Modifier,
-    color: TextColor = TextColor.Primary,
     typography: TextStyle = LocalTextStyle.current,
     fontStyle: FontStyle? = null,
     fontWeight: FontWeight? = null,
@@ -27,7 +24,6 @@ fun SnakeText(
 ) {
     Text(
         text = text,
-        color = color.toComposeColor(),
         style = typography,
         textAlign = textAlign,
         overflow = overflow,
@@ -36,23 +32,4 @@ fun SnakeText(
         fontWeight = fontWeight,
         modifier = modifier,
     )
-}
-
-enum class TextColor {
-    Primary,
-    Secondary,
-    Tertiary,
-    InversePrimary,
-    Error,
-    Success,
-}
-
-@Composable
-private fun TextColor.toComposeColor(): Color = when (this) {
-    TextColor.Primary -> MaterialTheme.colorScheme.primary
-    TextColor.Secondary -> MaterialTheme.colorScheme.secondary
-    TextColor.Tertiary -> MaterialTheme.colorScheme.tertiary
-    TextColor.InversePrimary -> MaterialTheme.colorScheme.inversePrimary
-    TextColor.Error -> MaterialTheme.colorScheme.error
-    TextColor.Success -> MaterialTheme.colorScheme.success
 }
