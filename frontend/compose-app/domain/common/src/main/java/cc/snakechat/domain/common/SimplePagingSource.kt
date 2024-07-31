453dc337431c758d205b7fca2a6f43362aa96ff3
Kshitij Patil <kshitijpatil98@gmail.com>
2024-07-31 22:49:14 +0530

    Extract generic paging source -> SimplePagingSource
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

---
+++ frontend/compose-app/domain/common/src/main/java/cc/snakechat/domain/common/SimplePagingSource.kt
@@ -0,0 +1,21 @@
+package cc.snakechat.domain.common
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+
+class SimplePagingSource<Req, Key : Any, Value : Any>(
+    private val feedFetcher: suspend (Req) -> LoadResult<Key, Value>,
+    private val requestMapper: (LoadParams<Key>) -> Req,
+) : PagingSource<Key, Value>() {
+
+    override fun getRefreshKey(state: PagingState<Key, Value>): Key? {
+        val anchor = state.anchorPosition ?: return null
+        val page = state.closestPageToPosition(anchor) ?: return null
+        return page.nextKey
+    }
+
+    override suspend fun load(params: LoadParams<Key>): LoadResult<Key, Value> {
+        val request = requestMapper(params)
+        return feedFetcher(request)
+    }
+}
