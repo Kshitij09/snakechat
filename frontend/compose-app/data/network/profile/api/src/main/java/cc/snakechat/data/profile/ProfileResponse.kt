2c5017dc73ac54ced0e9bdeaccd9958ba7a8846a
Kshitij Patil <kshitijpatil98@gmail.com>
2024-08-04 17:54:25 +0530

    Change counter type to Long
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- frontend/compose-app/data/network/profile/api/src/main/java/cc/snakechat/data/profile/ProfileResponse.kt
+++ frontend/compose-app/data/network/profile/api/src/main/java/cc/snakechat/data/profile/ProfileResponse.kt
@@ -5,14 +5,14 @@ import kotlinx.serialization.Serializable
 @Serializable
 class ProfileResponse(
     @SerialName("followers_count")
-    val followersCount: Int? = null,
+    val followersCount: Long? = null,
     @SerialName("following_count")
-    val followingCount: Int? = null,
+    val followingCount: Long? = null,
     val id: String? = null,
     @SerialName("post_thumbnails")
     val postThumbnails: List<PostThumbnail>? = null,
     @SerialName("posts_count")
-    val postsCount: Int? = null,
+    val postsCount: Long? = null,
     @SerialName("profile_url")
     val profileUrl: String? = null,
     val status: String? = null,
--- frontend/compose-app/domain/profile/src/main/kotlin/cc/snakechat/domain/profile/ProfileResponse.kt
+++ frontend/compose-app/domain/profile/src/main/kotlin/cc/snakechat/domain/profile/ProfileResponse.kt
@@ -1,11 +1,11 @@
 package cc.snakechat.domain.profile
 
 class Profile(
-    val followersCount: Int = 0,
-    val followingCount: Int = 0,
+    val followersCount: Long = 0,
+    val followingCount: Long = 0,
     val id: String,
     val postThumbnails: List<PostThumbnail> = emptyList(),
-    val postsCount: Int = 0,
+    val postsCount: Long = 0,
     val profileUrl: String? = null,
     val status: String? = null,
     val username: String
