cc9e28f5fdcfb4cc834ea61c2427bf83e2ba580c
Kshitij Patil <kshitijpatil98@gmail.com>
2024-08-04 16:58:05 +0530

    Add fake profile implementation
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- frontend/compose-app/data/network/fake/build.gradle.kts
+++ frontend/compose-app/data/network/fake/build.gradle.kts
@@ -8,6 +8,8 @@ android.namespace = "cc.snakechat.data"
 dependencies {
     api(projects.data.network.feed.api)
     api(projects.data.network.post.api)
+    api(projects.data.network.profile.api)
+    api(libs.result)
     implementation(libs.kotlinx.serialization)
 }
 
---
+++ frontend/compose-app/data/network/fake/src/main/kotlin/cc/snakechat/data/profile/FakeProfileApi.kt
@@ -0,0 +1,17 @@
+package cc.snakechat.data.profile
+
+import android.content.Context
+import cc.snakechat.data.network.NetworkError
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+
+class FakeProfileApi(private val context: Context, private val json: Json) : ProfileApi {
+    override suspend fun getProfile(userId: String): Result<ProfileResponse, NetworkError> {
+        return context.assets.open("profile/profile_1.json").use {
+            val response = json.decodeFromStream<ProfileResponse>(it)
+            Ok(response)
+        }
+    }
+}
