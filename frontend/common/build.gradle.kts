50569273f8649d98a3c7423c675ca6c1bf7c967d
Kshitij Patil <kshitijpatil98@gmail.com>
2024-10-06 23:10:44 +0530

    Configure group and version for all common modules
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- frontend/common/build.gradle.kts
+++ frontend/common/build.gradle.kts
@@ -7,3 +7,8 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
 }
+
+allprojects {
+    group = "cc.snakechat"
+    version = "1.0.0"
+}
