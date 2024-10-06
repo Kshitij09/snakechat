929ccca1e3bf16e53b7f2700e3c8b32de61b64cf
Kshitij Patil <kshitijpatil98@gmail.com>
2024-10-06 20:05:44 +0530

    Init composite build, move build-logic to top level

---
+++ frontend/settings.gradle.kts
@@ -0,0 +1,19 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+rootProject.name = "frontend"
+
